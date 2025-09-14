240dedbb953f782c36e919d4446340c8c9c75e0e
OCHBADRAKH <daikaochbadrah@gmail.com>
2025-09-14 18:27:46 +0800

    first commit

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { ArrowRight, CalendarIcon, ClockIcon } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+
+const HeroSection = () => {
+  const navigate = useNavigate()
+  return (
+    <div
+        className="flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-cover bg-center h-screen"
+        style={{ backgroundImage: "url('/backgroundImage.png')" }}
+    >
+        <img src={assets.marvelLogo} alt="" className='max-h-11 lg:h-11 mt-20'/>
+        <h1 className='text-6xl md:text-[70px] md:leading-18 font-semibold max-w-110'>Кино орон зай</h1>
+        <div className='flex items-center gap-4 text-gray-300'>
+            <span>Action | Adventure | Comedy</span>
+            <div className='flex items-center gap-1'>
+                <CalendarIcon className='w-4.5 h-4.5'/> 2018
+            </div>
+            <div className='flex items-center gap-1'>
+                <ClockIcon className='w-4.5 h-4.5'/> 2h 8m
+            </div>
+        </div>
+        <p className='max-w-md text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis temporibus et consequuntur alias fuga libero praesentium eum vitae dicta, reprehenderit voluptatum, placeat id expedita voluptates ipsam inventore aliquam dolorum, quo sunt error nostrum tenetur eligendi doloribus vero? Similique modi distinctio deserunt rerum temporibus saepe officiis?</p>
+        <button onClick={() => navigate('/movies')} className='flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>
+            Explorer Movies
+            <ArrowRight className='w-5 h-5'/>
+        </button>
+    </div>
+  )
+}
+
+export default HeroSection
